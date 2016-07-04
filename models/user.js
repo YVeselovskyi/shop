b0e642de4009e302b3b1d794cc6b9a54c909395c
Веселовский Ярослав < >
2016-07-04 20:19:14 +0300

    'Mongolab connection'

--- models/user.js
+++ models/user.js
@@ -1,3 +1,4 @@
+'use strict'
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt-nodejs');
 const Schema = mongoose.Schema;
@@ -18,13 +19,24 @@ let UserSchema = new Schema({
   }]
 })
 
-
-
-
-
-
 // Hash password
-
-
+UserSchema.pre('save', function(next) {
+  let user = this;
+  if(!user.isModified('password')) return next();
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err);
+    bcrypt.hash(user.password, salt, null, (err, hash) => {
+      if (err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
+})
 
 // Compare passwords in db and user password
+
+UserSchema.methods.comparePassword = (password) =>  {
+  return bcrypt.compareSync(password, this.password);
+}
+
+module.exports = mongoose.model('User' , UserSchema);
--- server.js
+++ server.js
@@ -2,18 +2,44 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
+const User = require('./models/user');
+
+mongoose.connect('mongodb://root:abc123@ds011725.mlab.com:11725/shop' , err => {
+  if(err) {
+    console.log(err);
+  } else {
+    console.log('Connected to database!')
+  }
+})
 
 //Middleware
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/' , (req, res) => {
-  res.json(`My name is Bob`);
+app.post('/create-user' , (req, res, next) =>{
+  let user = new User();
+
+  user.profile.name = req.body.name;
+  user.password = req.body.password;
+  user.email = req.body.email;
+
+  user.save((err) => {
+     if (err) return next(err);
+     res.json('Successfully creater a new user!')
+  });
 })
 
-app.get('/parrot' , (req, res) => {
-  res.json(`My name is Parrot`);
-})
+// app.get('/' , (req, res) => {
+//   res.json(`My name is Bob`);
+// })
+//
+// app.get('/parrot' , (req, res) => {
+//   res.json(`My name is Parrot`);
+// })
 
 
 app.listen(3000, err => {
